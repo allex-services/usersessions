95ff65dd18551dc12248155dfeab1bd83813e082
Andrija Petrovic <andrija.hers@gmail.com>
2022-09-13 13:16:08 +0200

    primaryKey fix + sql support

--- storagedescriptor.js
+++ storagedescriptor.js
@@ -1,20 +1,30 @@
 module.exports = {
-  primaryKey: 'session',
   record:{
+    primaryKey: 'session',
     fields:[{
       name: 'session',
-      type: 'string'
+      type: 'string',
+      sqltype: 'nvarchar[50]',
+      postgresqltype: 'text',
+      nullable: false
     },{
       name: 'created',
       default: '{{Date.now()}}',
-      type: 'number'
+      type: 'number',
+      sqltype: 'bigint',
+      nullable: false
     },{
       name: 'lastused',
       default: '{{Date.now()}}',
-      type: 'number'
+      type: 'number',
+      sqltype: 'bigint',
+      nullable: false
     },{
       name: 'username',
-      type: 'string'
+      type: 'string',
+      sqltype: 'nvarchar[100]',
+      postgresqltype: 'text',
+      nullable: false
     }]
   }
 };
